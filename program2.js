e5814e341b4515404e86fc86fe2caebb4eef48e1
Kirti Rajput <kirti.0112cs221071@gmail.com>
2024-09-25 14:37:55 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,10 +1,33 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
-    
-};
+function romanToInt(s) {
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
 
+    let total = 0;
+    let prevValue = 0;
 
-module.exports={romanToInt}
+    for (let i = s.length - 1; i >= 0; i--) {
+        const currentValue = romanMap[s[i]];
+        
+        if (currentValue < prevValue) {
+            total -= currentValue; // Subtract if the current value is less than the previous
+        } else {
+            total += currentValue; // Add otherwise
+        }
+        
+        prevValue = currentValue; // Update previous value
+    }
+
+    return total;
+}
+
+// Example usage:
+console.log(romanToInt("III"));     // Output: 3
+console.log(romanToInt("LVIII"));   // Output: 58
+console.log(romanToInt("MCMXCIV")); // Output: 1994
