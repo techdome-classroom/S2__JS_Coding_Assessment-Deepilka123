308212114e999d00366537787ea4654f74d8e362
Kirti Rajput <kirti.0112cs221071@gmail.com>
2024-09-25 14:24:54 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,11 +1,31 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
-    
-};
+function isValid(s) {
+    const stack = [];
+    const map = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
 
-module.exports = { isValid };
+    for (let char of s) {
+        if (map[char]) {
+            // If the character is an opening bracket, push it to the stack
+            stack.push(char);
+        } else {
+            // If the character is a closing bracket
+            if (stack.length === 0 || map[stack.pop()] !== char) {
+                return false; // Return false if not valid
+            }
+        }
+    }
+
+    // If the stack is empty, all brackets were matched
+    return stack.length === 0;
+}
+
+// Example Usage:
+console.log(isValid("()")); // Output: true
+console.log(isValid("()[]{}")); // Output: true
+console.log(isValid("(]")); // Output: false
+
 
 
