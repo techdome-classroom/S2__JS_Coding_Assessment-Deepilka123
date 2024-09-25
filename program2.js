c43fdf5105a16ff039091f9da5d6b1c6c671382f
Kirti Rajput <kirti.0112cs221071@gmail.com>
2024-09-25 14:42:11 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -30,6 +30,6 @@ module.exports = romanToInt; // Yeh line add karein
 
 
 // Example usage:
-console.log(romanToInt("III"));     // Output: 3
-console.log(romanToInt("LVIII"));   // Output: 58
-console.log(romanToInt("MCMXCIV")); // Output: 1994
+console.log(romanToInt("III"));     
+console.log(romanToInt("LVIII"));   
+console.log(romanToInt("MCMXCIV")); 
